b0a2c0cb1103b105578c16961161d8bc2dffeda9
Jhack Costa <jhacktibia@gmail.com>
2023-08-09 09:49:56 -0300

    Organizando pastas

--- app/dist/js/app.js
+++ app/dist/js/app.js
@@ -4,6 +4,7 @@ const form = document.querySelector(".form");
 if (form) {
     form.addEventListener("submit", e => {
         e.preventDefault();
+        console.log("Teste");
         controller.adiciona();
     });
 }
--- servidor-api/server.js
+++ servidor-api/server.js
@@ -1,6 +1,8 @@
+
 var http = require('http')
     ,app = require('./config/express');
 
+    console.log("Iniciando Servidor...");
 http.createServer(app).listen(8080, function() {
     console.log('Servidor escutando na porta: ' + this.address().port);
 });
